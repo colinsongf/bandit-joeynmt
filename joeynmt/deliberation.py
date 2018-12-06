correct batch normalization of loss; added standard xent

joeynmt/deliberation.py
@@ -53,6 +53,11 @@ class DeliberationModel(nn.Module):
         self.bos_index = self.trg_vocab.stoi[BOS_TOKEN]
         self.pad_index = self.trg_vocab.stoi[PAD_TOKEN]
         self.eos_index = self.trg_vocab.stoi[EOS_TOKEN]
+        self.register_buffer('total_cost', torch.zeros([1]))
+        self.register_buffer('total_samples', torch.zeros([1]))
+
+        self.criterion = nn.NLLLoss(ignore_index=self.pad_index, reduction='none')
+
 
     def forward(self, src, trg_input, src_mask, src_lengths):
         """
@@ -152,7 +157,7 @@ class DeliberationModel(nn.Module):
                              unrol_steps=unrol_steps,
                              hidden=decoder_hidden)
 
-    def get_loss_for_batch(self, batch, criterion):
+    def get_loss_for_batch(self, batch):
         """
         Compute non-normalized loss and number of tokens for a batch
         :param batch:
@@ -174,21 +179,53 @@ class DeliberationModel(nn.Module):
         # 3. shared encoder: sum of both
         # grad( log p_d2(y| y_d1, x)) + grad(log p_d1(y_d1 | x) * no_grad (log p_d2(y | y_d1, x))
 
-        # d1
-        d1_log_probs = F.log_softmax(d1_output, dim=-1)
-        # pretend targets are sampled
-        d1_pred_logprobs = criterion(input=d1_log_probs.contiguous().view(-1, d1_log_probs.size(-1)), target=d1_predictions.contiguous().view(-1))
-
         # d2
         d2_log_probs = F.log_softmax(d2_out, dim=-1)
-        # standard xent
-        d2_ref_logprobs = criterion(input=d2_log_probs.contiguous().view(-1, d2_log_probs.size(-1)), target=batch.trg.contiguous().view(-1))
+        # standard xent, ignores padding
+        batch_size = batch.trg.shape[0]
+        # loss for each batch
+        d2_ref_neglogprobs = self.criterion(input=d2_log_probs.contiguous().view(-1, d2_log_probs.size(-1)), target=batch.trg.contiguous().view(-1)).view(batch_size, -1).sum(-1)
+        assert d2_ref_neglogprobs.shape == torch.Size([batch_size])
+
+        # d1
+        d1_log_probs = F.log_softmax(d1_output, dim=-1)
+        # pretend targets are sampled, ignores padding
+        # TODO include mask to exclude area after first eos?
+        d1_pred_neglogprobs = self.criterion(
+            input=d1_log_probs.contiguous().view(-1, d1_log_probs.size(-1)),
+            target=d1_predictions.contiguous().view(-1)).view(batch_size, -1).sum(-1)
+        assert d1_pred_neglogprobs.shape == torch.Size([batch_size])
+
+        # also add standard xent loss for 1s decoder to keep it stable
+        d1_ref_neglogprobs = self.criterion(input=d1_log_probs.contiguous().view(-1, d1_log_probs.size(-1)),
+            target=batch.trg.contiguous().view(-1)).view(batch_size, -1).sum(-1)
+        assert d1_ref_neglogprobs.shape == torch.Size([batch_size])
+
 
         # don't backprop through d2 for d1's parameters
-        d1_loss = d1_pred_logprobs * d2_ref_logprobs.detach()
-        d2_loss = d2_ref_logprobs
+        # d2's likelihood is the reward for d1 (padding not included) -> neg ll is cost, since we do minimization
+        #print("d2 neg log prob", d2_ref_neglogprobs)
+        #print("norm", d2_ref_neglogprobs/batch.ntokens)
+        #print("d1_pred_neglo", d1_pred_neglogprobs)
+        # FIXME neg log likelihood is huge in the beginning
+        cost = d2_ref_neglogprobs.detach()
 
-        batch_loss = d1_loss + d2_loss
+        # update running sums to compute baseline
+        #self.total_samples += batch.ntokens
+        #self.total_cost += cost
+        #baseline = self.total_cost/self.total_samples
+        #print(cost, baseline)
+        #cost = cost - baseline
+       # print(cost)
+
+        d1_loss = d1_pred_neglogprobs*cost + d1_ref_neglogprobs
+        d2_loss = d2_ref_neglogprobs
+        #print(d1_loss, d2_loss)
+
+        # TODO separate gradients like here?
+        # https://blog.millionintegrals.com/vel-pytorch-meets-baselines/
+
+        batch_loss = (d1_loss + d2_loss).sum()  # sum over batch
 
         # add lm loss (optional)
         if lm_output is not None:
@@ -205,7 +242,7 @@ class DeliberationModel(nn.Module):
             print("predicted", predictions[0])
             lm_logprobs = F.log_softmax(lm_output, dim=-1)
             # shift inputs to the left for loss targets, ignore last hidden state
-            lm_loss = criterion(
+            lm_loss = self.criterion(
                 input=lm_logprobs[:, :-1].contiguous().view(-1,
                                                             lm_logprobs.size(
                                                                 -1)),

joeynmt/model.py
@@ -108,6 +108,7 @@ class Model(nn.Module):
         self.bos_index = self.trg_vocab.stoi[BOS_TOKEN]
         self.pad_index = self.trg_vocab.stoi[PAD_TOKEN]
         self.eos_index = self.trg_vocab.stoi[EOS_TOKEN]
+        self.criterion = nn.NLLLoss(ignore_index=self.pad_index, reduction='sum')
 
     def forward(self, src, trg_input, src_mask, src_lengths):
         """
@@ -162,7 +163,7 @@ class Model(nn.Module):
                             unrol_steps=unrol_steps,
                             hidden=decoder_hidden)
 
-    def get_loss_for_batch(self, batch, criterion):
+    def get_loss_for_batch(self, batch):
         """
         Compute non-normalized loss and number of tokens for a batch
         :param batch:
@@ -179,7 +180,7 @@ class Model(nn.Module):
         log_probs = F.log_softmax(out, dim=-1)
 
         # compute batch loss
-        batch_loss = criterion(
+        batch_loss = self.criterion(
             input=log_probs.contiguous().view(-1, log_probs.size(-1)),
             target=batch.trg.contiguous().view(-1))
         # return batch loss = sum over all elements in batch that are not pad

joeynmt/training.py
@@ -35,7 +35,6 @@ class TrainManager:
         self.model = model
         self.pad_index = self.model.pad_index
         self.bos_index = self.model.bos_index
-        criterion = nn.NLLLoss(ignore_index=self.pad_index, reduction='sum')
         self.learning_rate_min = train_config.get("learning_rate_min", 1.0e-8)
         if train_config["loss"].lower() not in ["crossentropy", "xent",
                                                 "mle", "cross-entropy"]:
@@ -143,7 +142,6 @@ class TrainManager:
         self.shuffle = train_config.get("shuffle", True)
         self.epochs = train_config["epochs"]
         self.batch_size = train_config["batch_size"]
-        self.criterion = criterion
         self.normalization = train_config.get("normalization", "batch")
         self.steps = 0
         # stop training if this flag is True by reaching learning rate minimum
@@ -269,6 +267,10 @@ class TrainManager:
                     new_param_dict["decoder2.d1_attention.query_layer.weight"] = init(torch.empty_like(self.model.decoder2.d1_attention.query_layer.weight))
                     new_param_dict["decoder2.d1_attention.energy_layer.weight"] = init(torch.empty_like(self.model.decoder2.d1_attention.energy_layer.weight))
 
+                if "total_sample" not in param_dict:
+                    # buffer for reward baselines
+                    new_param_dict["total_samples"] = torch.zeros_like(self.model.total_samples)
+                    new_param_dict["total_cost"] = torch.zeros_like(self.model.total_cost)
                 self.model.load_state_dict(new_param_dict)
         else:
             self.model.load_state_dict(param_dict)
@@ -375,8 +377,7 @@ class TrainManager:
             eval_metric=self.eval_metric,
             level=self.level, model=self.model,
             use_cuda=self.use_cuda,
-            max_output_length=self.max_output_length,
-            criterion=self.criterion)
+            max_output_length=self.max_output_length)
         if isinstance(self.model, DeliberationModel):
             (valid_score,
              aux_valid_score), valid_loss, valid_ppl, valid_sources, \
@@ -535,8 +536,7 @@ class TrainManager:
         :param batch:
         :return:
         """
-        batch_loss = self.model.get_loss_for_batch(
-            batch=batch, criterion=self.criterion)
+        batch_loss = self.model.get_loss_for_batch(batch=batch)
 
         # normalize batch loss
         if self.normalization == "batch":
@@ -687,7 +687,7 @@ def train(cfg_file):
                 data=test_data, batch_size=trainer.batch_size,
                 eval_metric=trainer.eval_metric, level=trainer.level,
                 max_output_length=trainer.max_output_length,
-                model=model, use_cuda=trainer.use_cuda, criterion=None,
+                model=model, use_cuda=trainer.use_cuda,
                 beam_size=beam_size, beam_alpha=beam_alpha)
 
             if "trg" in test_data.fields: