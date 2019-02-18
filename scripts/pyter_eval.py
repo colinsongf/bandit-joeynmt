ce4ec8266c6b3e2b79ee5269c15b326504975534
Julia Kreutzer <juliakreutzer1@aol.com>
2019-02-18 14:13:55 +0100

    pyter eval script

---
+++ scripts/pyter_eval.py
@@ -0,0 +1,22 @@
+import pyter
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+    refs = []
+    with open(sys.argv[1], "r") as ref:
+        for line in ref:
+            refs.append(line.strip().split())
+    hyps = []
+    for line in sys.stdin:
+        hyps.append(line.strip().split())
+
+    assert len(refs) == len(hyps)
+    print("Scoring {} refs/hyps with PyTER.".format(len(refs)))
+
+    ters = []
+    for h, r in zip(hyps, refs):
+        ters.append(pyter.ter(inputwords=h, refwords=r))
+    ter = np.mean(ters)
+
+    print("avg TER: ", ter)
