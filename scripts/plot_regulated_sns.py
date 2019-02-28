0b2de1e7fabd2cb9aaa602eb5e65249607fcc7b8
Julia Kreutzer <juliakreutzer1@aol.com>
2019-02-28 01:15:04 +0100

    sd instead of ci

--- scripts/plot_regulated_sns.py
+++ scripts/plot_regulated_sns.py
@@ -131,7 +131,7 @@ def plot_models(models, df, x_value, y_value, output_path, plot_sup, lim_x):
 
         #fmri = sns.load_dataset("fmri")
         #print(fmri)
-    ax = sns.lineplot(x=x_value, y=y_value, data=df, hue="type", style="type", legend="brief")
+    ax = sns.lineplot(x=x_value, y=y_value, data=df, hue="type", style="type", legend="brief", ci="sd")
         #ax = sns.lineplot(x=xs, y=ys, markers=None, labels=model_name)
 
         #f.plot(xs, ys)
